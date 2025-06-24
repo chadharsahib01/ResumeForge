d9a0b5cf648d64723d00ff11d7345dd74d6b899e
fazool websites <casualloginn@gmail.com>
2025-06-24 03:53:26 +0000

    Now how can i make its apk?

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cvbnao.app',
+  appName: 'CV Bnao',
+  webDir: 'out',
+  bundledWebRuntime: false,
+};
+
+export default config;
